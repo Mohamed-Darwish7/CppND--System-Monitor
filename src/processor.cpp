80be9a2a9dd59642259e2fbd392c03eec5e190b0
Mohamed Darwish <63372384+Mohamed-Darwish7@users.noreply.github.com>
2021-08-15 22:03:28 +0200

    Update processor.cpp

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,31 @@
 #include "processor.h"
 
 // TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+float Processor::Utilization() { 
+  std::vector<std::string> Jiffies = LinuxParser::CpuUtilization();
+  int count {0};
+  
+  for(auto Jiffie : Jiffies){
+  	if(count != 3 || count != 4){
+    	Nonidle += std::stof(Jiffie);
+    }
+    if(count == 3 || count == 4){
+    	idle += std::stof(Jiffie);
+    }
+    count ++;
+  }
+  
+  bool Current(true);
+  if(Current){
+    total = Nonidle + idle;
+  	totald = total - Prevtotal;
+    idled = idle - Previdle;
+    CpuPercentage = (totald - idled) / totald;
+    
+    Previdle = idle;
+    PrevNonidle = Nonidle;
+    Prevtotal = total;  
+  }
+  
+  return CpuPercentage;
+}
